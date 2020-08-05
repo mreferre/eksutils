bf3b8d092b891196eb6e3697766d2ef18d373799
Massimo Re Ferre <massimo@it20.info>
2020-08-05 12:01:50 +0200

    Fixed the utilsversions.sh script to set the docker experimental variable needed for docker-ecs

--- utilsversions.sh
+++ utilsversions.sh
@@ -134,7 +134,7 @@ logger "green" "checking docker-compose..."
 docker-compose --version
 errorcheck docker-compose
 logger "green" "checking docker-ecs..."
-docker ecs version
+export DOCKER_CLI_EXPERIMENTAL=enabled && docker ecs version
 errorcheck docker-ecs
 logger "green" "checking kind..."
 kind --version
